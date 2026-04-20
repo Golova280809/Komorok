f2210fc9f6ca5e5ad725a42ececf3b05afedd28f
Skaut9910 <150506181+Golova280809@users.noreply.github.com>
2026-04-20 15:02:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const backToTopBtn = document.getElementById("backToTop");
 
-        window.onscroll = function() {
-            if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-                backToTopBtn.classList.add("show");
-            } else {
-                backToTopBtn.classList.remove("show");
-            }
-        };
-
-        backToTopBtn.onclick = function() {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            });
-        };
 (function() {
             // Кнопка "Наверх"
             const backToTop = document.getElementById('backToTop');
