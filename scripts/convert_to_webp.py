f23f8da57d1bc52a9361f80d108cfdc6e7001d1e
Skaut9910 <150506181+Golova280809@users.noreply.github.com>
2026-08-15 18:45:05 +0900

    Исправление пути

--- scripts/convert_to_webp.py
+++ scripts/convert_to_webp.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-IMG_DIR = 'komorium/bazanovo/album/img/'
+IMG_DIR = 'Komorium/bazanovo/album/img/'
 SUPPORTED = ('.png', '.jpg', '.jpeg')
 
 for filename in os.listdir(IMG_DIR):
